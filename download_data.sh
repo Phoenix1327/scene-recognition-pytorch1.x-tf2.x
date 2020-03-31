2f94d8252db71d61a462de76e01cd405eecc19ad
HuaizhengZhang <huaizhen001@e.ntu.edu.sg>
2020-03-31 21:25:02 +0700

    start to download the data

---
+++ download_data.sh
@@ -0,0 +1,15 @@
+
+
+echo "Download the Places365 standard easyformat"
+
+wget http://data.csail.mit.edu/places/places365/places365standard_easyformat.tar
+
+echo "Start to extract files"
+
+tar -zxvf places365standard_easyformat.tar
+
+echo "Finish extracting files"
+
+echo "Download the category index file"
+
+wget https://github.com/CSAILVision/places365/blob/master/categories_places365.txt
